055c40f794eaf7a0f4fa7f36f0b6c442de2ab899
DmitryShirkavets <46999066+DmitryShirkavets@users.noreply.github.com>
2021-09-11 23:06:11 +0300

    update

---
+++ 01/js/script.js
@@ -0,0 +1,74 @@
+// T1
+console.log('Dima');
+
+// T2
+console.log(10);
+
+// T3
+console.log('Добро ' + 'пожаловать ' + 'на курс');
+
+// T5
+// alert(2019-200);
+
+// T6
+document.getElementById('one').innerHTML = 'Hello World!';
+
+// T7
+document.getElementById('two').innerHTML = 12*12;
+
+// T8
+document.querySelector('.one').innerHTML = 'Hello World!';
+
+// T9
+document.querySelector('span').innerHTML = 'world';
+
+// T10
+document.querySelector('.three').innerHTML = '<h3>Privet</h3>';
+
+// T11
+document.querySelector('.four').innerHTML = '<h4>Privet</h4>';
+document.querySelector('.four').innerHTML += '<p>Andrey</p>';
+
+// T12
+let a = document.querySelector('.five');
+a.innerHTML = 3.1415;
+
+// T13
+let div7 = document.querySelector('.seven');
+div7.innerHTML = '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">';
+
+// T14
+let z1 = 6;
+let z2 = 3;
+document.querySelector('.z1z2').innerHTML = z1*z2;
+
+// T15
+let y1 = 6;
+let y2 = 3;
+document.querySelector('.y1y2').innerHTML = z1/z2;
+
+// T16
+x1 ='Hello ', x2 = 5;
+document.querySelector('.t16').innerHTML = x1+x2;
+
+// T17
+let d1 = document.querySelector('.test1');
+console.log(d1);
+
+// T18
+let d2 = document.querySelector('.test2');
+console.log(d2);
+d2 = 5;
+console.log(d2);
+
+// T19
+let divS3 = document.querySelector('.s3');
+console.log(divS3);
+divS3 += document.querySelector('.s4');
+console.log(divS3);
+
+// T20
+let body = document.querySelector('body');
+console.log(body);
+// body.innerHTML = '';
+console.log(body);
